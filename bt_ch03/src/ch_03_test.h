d7990039022b1d3c8559e21c1dc2cd41f2f40cbe
SilasYoome <axxx2934@gmail.com>
2021-06-04 14:31:03 +0800

    add ch04

---
+++ bt_ch03/src/ch_03_test.h
@@ -0,0 +1,89 @@
+#include <behaviortree_cpp_v3/action_node.h>
+using namespace BT;
+
+/* This tutorial will teach you how to deal with ports when its
+ *  type is not std::string.
+ *  教你使用當資料型態不是std::string時的端口要如何宣告
+*/
+
+
+
+// We want to be able to use this custom type
+// 自定義資料型態
+struct Position2D { double x,y; };
+
+// It is recommended (or, in some cases, mandatory) to define a template
+// specialization of convertFromString that converts a string to Position2D.
+// 推薦使用(在某個情況下是強制使用)樣板(template)，
+// 將字串(std::string)用convertFromString將資料轉變成Position2D內宣告的資料型態
+namespace BT
+{
+template <> inline Position2D convertFromString(StringView str) // StringView是C++11版本的std::string_view
+{
+    printf("Converting string: \"%s\"\n", str.data() ); // 終端機顯示現在Position2D的內容，目前的型態是std::strinr
+
+    // real numbers separated by semicolons
+    // splitString()為字串分割，根據;來分割字串
+    auto parts = splitString(str, ';');
+    if (parts.size() != 2)
+    {
+        throw RuntimeError("invalid input)");
+    }
+    else{
+        // 將字串分割完畢之後，就可以使用convertFromeString<資料型態>來將字串改成我們所需要的資料型態
+        Position2D output;
+        output.x     = convertFromString<double>(parts[0]);
+        output.y     = convertFromString<double>(parts[1]);
+        return output;
+    }
+}
+} // end namespace BT
+
+// write int a port
+class CalculateGoal: public SyncActionNode
+{
+public:
+    CalculateGoal(const std::string& name, const NodeConfiguration& config):
+        SyncActionNode(name,config)
+    {}
+
+    NodeStatus tick() override
+    {
+        Position2D mygoal = {1.1, 2.3};// 將Position2D賦予1.1與2.3兩個值
+        setOutput("goal", mygoal);
+        return NodeStatus::SUCCESS;
+    }
+    static PortsList providedPorts()
+    {
+        return { OutputPort<Position2D>("goal") };
+    }
+};
+
+
+// read from a port
+class PrintTarget: public SyncActionNode
+{
+public:
+    PrintTarget(const std::string& name, const NodeConfiguration& config):
+        SyncActionNode(name,config)
+    {}
+
+    NodeStatus tick() override
+    {
+        auto res = getInput<Position2D>("target");  // 確認運作正常
+        if( !res )
+        {
+            throw RuntimeError("error reading port [target]:", res.error() );
+        }
+        Position2D goal = res.value();  // Postition2D放入值
+        printf("Target positions: [ %.1f, %.1f ]\n", goal.x, goal.y );
+        return NodeStatus::SUCCESS;
+    }
+
+    static PortsList providedPorts()
+    {
+        // Optionally, a port can have a human readable description
+        const char*  description = "Simply print the target on console...";
+        return { InputPort<Position2D>("target", description) };
+    }
+};
--- bt_ch04/src/main.cpp
+++ bt_ch04/src/main.cpp
@@ -1,19 +1,8 @@
-/*
-input port & output port不同於ch02，端口型態為std::string
-ch03主要是教說如何宣告成不同型態的端口，int、long、double、bool、NodeStatus等
-
-除此之外，本節還教了兩種資料寫入方式
-一種是用output port來寫入
-一種是用xml檔案裡面blackboard的功能來寫入
-
-*/
-
 #include <ros/ros.h>
 #include <behaviortree_cpp_v3/bt_factory.h>
 #include <behaviortree_cpp_v3/loggers/bt_cout_logger.h>
 #include <behaviortree_cpp_v3/loggers/bt_zmq_publisher.h>
 
-#include "ch_03_test.h"
 
 using namespace BT;
 int main(int argc,char **argv){
@@ -22,9 +11,10 @@ int main(int argc,char **argv){
     ros::NodeHandle nh("~");
     std::string xml_filename;
 
-    nh.param<std::string>("file",xml_filename,"/home/behaviortree_test/src/bt_ch03/bt_ch03_xml/bt_ch03_xml.xml");
+    nh.param<std::string>("file",xml_filename,"/home/behaviortree_test/src/bt_ch04/bt_ch04_xml/bt_ch04_xml.xml");
     ROS_INFO("LoadingXML : %s",xml_filename.c_str());
 
+/*
     BehaviorTreeFactory factory;
     factory.registerNodeType<CalculateGoal>("CalculateGoal");
     factory.registerNodeType<PrintTarget>("PrintTarget");
@@ -40,6 +30,6 @@ int main(int argc,char **argv){
         status = tree.rootNode()->executeTick();  // status變成現在樹正在運行的節點，並顯示它目前運行的狀態
         // Sleep 100 milliseconds
         std::this_thread::sleep_for(std::chrono::milliseconds(100)); // 等個0.1秒
-    }
+    }*/
   return 0;
 }
