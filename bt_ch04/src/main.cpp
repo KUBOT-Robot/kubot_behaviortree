54135845b091787e062631ccc0faa6a7e6581c35
SilasYoome <axxx2934@gmail.com>
2021-06-07 16:58:25 +0800

    add all ch04

--- bt_ch04/src/main.cpp
+++ bt_ch04/src/main.cpp
@@ -1,3 +1,46 @@
+/*
+Sequeue與ReactiveSequeue兩種不同框架下的Sequeue節點運行狀態
+Sequeue的運作原理是，tick子節點時，
+若子節點回傳FAILURE   ，則在下次運行的時候，從頭開始運行
+若子節點回傳RUNNINT，則在下次運行的時候，從剛剛回傳RUNNING的節點開始運行
+
+ReactiveSequeue的運作原理是，tick子節點時，
+若子節點回傳FAILURE   ，則在下次運行的時候，從頭開始運行
+若子節點回傳RUNNING，則在下次運行的時候，從頭開始運行
+
+因此這章節有兩個xml檔，
+bt_ch04_xml.xml為ReactiveSequeue
+bt_ch04_1_xml.xml為Sequeue
+
+兩者運行的結果可以看出其不同
+
+Sequeue的運行結果會顯示：
+  -No:1
+  -[ CheckBattery OK ]
+  -Robot says : mission started...
+  -[ MoveBase: STARTED ]. goal: x=1 y=2.0 theta=3.00
+  -No:2
+  -[ MoveBase: FINISHED ]
+  -No:3
+  -Robot says : mission copleted!
+可以注意到No:1與No:2的時候，MoveBase回傳了RUNNING，No:3的時候才回傳了SUCCESS，BatteryOK只執行了一次
+
+ReactiveSequeue的運行結果會顯示：
+-No:1
+-[ CheckBattery OK ]
+-Robot says : mission started...
+-[ MoveBase: STARTED ]. goal: x=1 y=2.0 theta=3.00
+-No:2
+-[ CheckBattery OK ]
+-Robot says : mission started...
+-[ MoveBase: FINISHED ]
+-No:3
+-[ CheckBattery OK ]
+-Robot says : mission started...
+-Robot says : mission copleted!
+可以注意到BatteryOK被執行的三次，意思是執行了了三次，而且三次都是重頭開始執行
+
+*/
 #include <ros/ros.h>
 #include <behaviortree_cpp_v3/bt_factory.h>
 #include <behaviortree_cpp_v3/loggers/bt_cout_logger.h>
@@ -26,15 +69,18 @@ int main(int argc,char **argv){
 
     auto tree = factory.createTreeFromFile(xml_filename);
 
-    StdCoutLogger logger_cout(tree);
+    //StdCoutLogger logger_cout(tree); //會將每個節點運行的狀態顯示在終端機上面，但這次的練習會造成畫面雜亂，所以先註解掉，需要的人再解除註解
 
 
-    NodeStatus status = NodeStatus::RUNNING;//預設節點robot_sequeue狀態目前為RUNNING
-     //Keep on ticking until you get either a SUCCESS or FAILURE state
-    while (ros::ok() && status == NodeStatus::RUNNING) {  // ROS運行OK以及robot_squeue狀態為RUNNING 就開始運行
-        status = tree.rootNode()->executeTick();  // status變成現在樹正在運行的節點，並顯示它目前運行的狀態
-        // Sleep 100 milliseconds
-        std::this_thread::sleep_for(std::chrono::milliseconds(100)); // 等個0.1秒
+    NodeStatus status = NodeStatus::RUNNING;
+    int i = 1;
+    while(ros::ok() && status == NodeStatus::RUNNING){
+      std::cout << "No:" << i << std::endl;
+      status = tree.rootNode()->executeTick();
+      std::this_thread::sleep_for(std::chrono::milliseconds(150)); 
+      i++;
+      if(i > 3) i = 1;
     }
+
   return 0;
 }
--- bt_ch04/src/move_base_action.h
+++ bt_ch04/src/move_base_action.h
@@ -7,6 +7,25 @@ struct Pose2D
     double x, y, theta;
 };
 
+namespace BT
+{
+    template <> inline Pose2D convertFromString(StringView str) // StringView是C++11版本的std::string_view
+    {
+        auto parts = splitString(str,';');
+        if(parts.size() != 3)
+        {
+            throw RuntimeError("invalid input");
+        }
+        else{
+            Pose2D output;
+            output.x =  convertFromString<double>(parts[0]);
+            output.y = convertFromString<double>(parts[1]);
+            output.theta = convertFromString<double>(parts[2]);
+            return output;
+        }
+    }
+}
+
 class MoveBaseAction : public AsyncActionNode
 {
   public:
@@ -22,6 +41,7 @@ class MoveBaseAction : public AsyncActionNode
     NodeStatus tick() override;
 
     // This overloaded method is used to stop the execution of this node.
+    // 這個成員主要是用來停止節點的執行
     void halt() override
     {
         _halt_requested.store(true);
@@ -47,14 +67,15 @@ NodeStatus MoveBaseAction::tick()
     _halt_requested.store(false);
     int count = 0;
 
-    // Pretend that "computing" takes 250 milliseconds.
+    // Pretend that "computing" takes 250 milliseconds. 假設運行時間需要250ms
     // It is up to you to check periodicall _halt_requested and interrupt
     // this tick() if it is true.
+    // 由我們檢查_halt_requested，在它為true時中斷此tick()
     while (!_halt_requested && count++ < 25)
     {
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
     }
 
     std::cout << "[ MoveBase: FINISHED ]" << std::endl;
-    return _halt_requested ? NodeStatus::FAILURE : NodeStatus::SUCCESS;
+    return _halt_requested ? NodeStatus::FAILURE: NodeStatus::SUCCESS;
 }
