c876c2d8402164a60627690116994c6fbb7f5984
SilasYoome <axxx2934@gmail.com>
2021-09-03 16:15:12 +0800

    update ch08

--- bt_ch08/src/main.cpp
+++ bt_ch08/src/main.cpp
@@ -12,7 +12,7 @@ int main(int argc,char **argv){
     ros::NodeHandle nh("~");
     std::string xml_filename;
 
-    nh.param<std::string>("file",xml_filename,"/home/behaviortree_test/src/bt_ch08/bt_ch08/bt_ch08_xml.xml");
+    nh.param<std::string>("file",xml_filename,"/home/behaviortree_test/src/bt_ch08/xml/bt_ch08_xml.xml");
     ROS_INFO("LoadingXML : %s",xml_filename.c_str());
 
     BehaviorTreeFactory factory;
@@ -27,15 +27,19 @@ int main(int argc,char **argv){
     factory.registerBuilder<Action_A>( "Action_A", builder_A);
     
     // Action_B用正常註冊就行，但我們依舊需要先呼叫Action_B::init()
-    //factory.registerNodeType<Action_B>( "Action_B" );
+    factory.registerNodeType<Action_B>( "Action_B" );
 
     auto tree = factory.createTreeFromText(xml_filename);
 
-    NodeStatus status = NodeStatus::RUNNING;
-
-    while (ros::ok() && status == NodeStatus::RUNNING) { 
-        status = tree.rootNode()->executeTick();
-        std::this_thread::sleep_for(std::chrono::milliseconds(100)); 
+     // Iterate through all the nodes and call init if it is an Action_B
+    for( auto& node: tree.nodes )
+    {
+        if( auto action_B_node = dynamic_cast<Action_B*>( node.get() ))
+        {
+            action_B_node->init( 69, 9.99, "interesting_value" );
+        }
     }
+
+     tree.tickRoot();
     return 0;
 }
