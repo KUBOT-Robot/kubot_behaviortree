36317b7639db184adf2b10f93d1c673b14f88ae1
SilasYoome <71079352+SilasYoome@users.noreply.github.com>
2021-09-07 08:39:13 +0800

    update main

---
+++ kubot_bt_test/src/main.cpp
@@ -0,0 +1,34 @@
+#include "movebase_client.h"
+#include "interrupt_event.h"
+
+#include <behaviortree_cpp_v3/bt_factory.h>
+#include <behaviortree_cpp_v3/loggers/bt_cout_logger.h>
+
+using namespace BT;
+
+int main(int argc, char **argv) {
+   ros::init(argc, argv, "test_bt");
+
+   ros::NodeHandle nh("~");
+   std::string xml_filename;
+   nh.param<std::string>("file",xml_filename,"/home/behaviortree_test/src/kubot_bt_test/xml/kubot_bt_test.xml");
+   ROS_INFO("Loading XML : %s" , xml_filename.c_str());
+
+   BehaviorTreeFactory factory;
+
+   factory.registerNodeType<MoveBase>("MoveBase");
+   factory.registerNodeType<InterruptEvent>("InterruptEvent");
+
+   auto tree = factory.createTreeFromFile(xml_filename);
+
+   StdCoutLogger logger_cout(tree);
+
+   NodeStatus status = NodeStatus::RUNNING;
+
+   while(ros::ok() && status == NodeStatus::RUNNING) {
+      status = tree.rootNode()->executeTick();
+      std::this_thread::sleep_for(std::chrono::milliseconds(100));
+   }
+
+   return 0;
+}
